1c2a5abd747347541afc386f8560aeddd604383c
Ramil-Karipov <omil8@yandex.ru>
2024-04-23 18:05:57 +0500

    10.4

--- src/test/java/ru/hogwarts/school/controller/StudentControllerWebMVCTest.java
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerWebMVCTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,6 +20,7 @@ import ru.hogwarts.school.repository.FacultyRepository;
 import ru.hogwarts.school.repository.StudentRepository;
 import ru.hogwarts.school.service.StudentService;
 
+import java.util.List;
 import java.util.Optional;
 
 @WebMvcTest(controllers = StudentController.class)
@@ -44,15 +46,59 @@ public class StudentControllerWebMVCTest {
         optionalStudent = Optional.of(student);
         Mockito.when(studentRepository.findById(ArgumentMatchers.anyLong()))
                 .thenReturn(optionalStudent);
+        Mockito.when(studentRepository.findAll())
+                .thenReturn(List.of(student));
     }
 
     @Test
-    void crud() throws Exception {
+    void create() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/student")
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .content(mapper.writeValueAsString(student)))
                 .andExpect(MockMvcResultMatchers.status().isOk());
         Mockito.verify(studentRepository).save(ArgumentMatchers.any(Student.class));
+
+    }
+
+    @Test
+    void update() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.request(HttpMethod.PUT, "/student/1")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(mapper.writeValueAsString(student)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        Mockito.verify(studentRepository).save(ArgumentMatchers.any(Student.class));
+
+    }
+
+    @Test
+    void delete() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.request(HttpMethod.DELETE, "/student/1")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(mapper.writeValueAsString(student)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        Mockito.verify(studentRepository).delete(ArgumentMatchers.any(Student.class));
+    }
+
+    @Test
+    void get() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/student/1")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("id").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("name").value("stud_name"))
+                .andExpect(MockMvcResultMatchers.jsonPath("age").value("20"));
+
+    }
+
+    @Test
+    void getAll() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/student")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("stud_name"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].age").value("20"));
 
     }
 }
