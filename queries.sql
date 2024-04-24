a287383566e667fe6f0e2e69fb1c4e76ebcb11d8
Ramil-Karipov <omil8@yandex.ru>
2024-04-24 12:44:53 +0500

    10.5

--- queries.sql
+++ queries.sql
@@ -18,3 +18,12 @@
 5. select *
    from student s
    order by age;
+
+
+   SELECT student.name, student.age, faculty.name
+   FROM student
+   INNER JOIN faculty ON student.faculty_id= faculty_id;
+
+   SELECT student
+   FROM student
+   INNER JOIN avatar a ON student.id= a.student_id;
