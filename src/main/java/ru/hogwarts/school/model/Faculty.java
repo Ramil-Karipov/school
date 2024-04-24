a1ca71cd2d099bce7a3437ec988bc75b01d5b3c8
Ramil-Karipov <omil8@yandex.ru>
2024-04-24 12:40:39 +0500

    10.5

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -23,6 +23,9 @@ public class Faculty {
     private List<Student> students;
 
     public Faculty(Long id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
     }
 
     public Long getId() {
--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -1,8 +1,10 @@
 package ru.hogwarts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.hogwarts.school.model.Student;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
@@ -10,5 +12,14 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByAge(int age);
 
     List<Student> findAllByAgeBetween(int min, int max);
+
+    @Query(value = "SELECT COUNT(*)FROM student", nativeQuery = true)
+    Integer getCountOfAllStudents();
+
+    @Query(value = "SELECT AVG(age)FROM student", nativeQuery = true)
+    Double getAverageAgeOfStudents();
+
+    @Query(value = "SELECT*FROM student ORDER BY id DESC LIMIT 5", nativeQuery = true)
+    Collection<Student> getLastFiveStudents();
 
 }
