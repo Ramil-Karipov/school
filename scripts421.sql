2076a29d059e799043dbe0d37b259e80d3520fa2
Ramil-Karipov <omil8@yandex.ru>
2024-05-03 10:22:33 +0500

    11.3

--- scripts421.sql
+++ scripts421.sql
@@ -1,4 +1,4 @@
-ALTER TABLE student ADD CONSTRAINT age_constraint(age>16);
+ALTER TABLE student ADD CONSTRAINT age_constraint CHECK(age>16);
 ALTER TABLE student ADD CONSTRAINT name UNIQUE(name);
 ALTER TABLE faculty ADD CONSTRAINT color_name_unique UNIQUE(name,color);
-ALTER TABLE student SET DEFAULT 20;
+ALTER TABLE student ALTER COLUMN age SET DEFAULT 20;
